46b7684d02ac609b022afac890ed329838b53a66
Dan Harris <danharris1606@gmail.com>
2020-12-17 21:46:02 +0000

    Add day 17

---
+++ day17/day17.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type State int
+
+const (
+	Inactive State = iota
+	Active
+)
+
+type Position struct {
+	x int
+	y int
+	z int
+	w int
+}
+
+func CountAdjacent(grid *map[Position]State, pos Position) int {
+	g := *grid
+	var adj int
+	for zI := pos.z - 1; zI <= pos.z+1; zI++ {
+		for yI := pos.y - 1; yI <= pos.y+1; yI++ {
+			for xI := pos.x - 1; xI <= pos.x+1; xI++ {
+				for wI := pos.w - 1; wI <= pos.w+1; wI++ {
+					p := Position{
+						x: xI,
+						y: yI,
+						z: zI,
+						w: wI,
+					}
+
+					if s, ok := g[p]; ok && p != pos && s == Active {
+						adj++
+					}
+				}
+			}
+		}
+	}
+
+	return adj
+}
+
+func CountActive(grid *map[Position]State) int {
+	var total int
+	for _, s := range *grid {
+		if s == Active {
+			total++
+		}
+	}
+	return total
+}
+
+func PadGrid(grid *map[Position]State, withW bool) {
+	var (
+		minX int = 1000
+		maxX int = -1000
+		minY int = 1000
+		maxY int = -1000
+		minZ int = 1000
+		maxZ int = -1000
+		minW int = 1000
+		maxW int = -1000
+	)
+	for p, _ := range *grid {
+		if p.x < minX {
+			minX = p.x
+		}
+		if p.x > maxX {
+			maxX = p.x
+		}
+		if p.y < minY {
+			minY = p.y
+		}
+		if p.y > maxY {
+			maxY = p.y
+		}
+		if p.z < minZ {
+			minZ = p.z
+		}
+		if p.z > maxZ {
+			maxZ = p.z
+		}
+		if p.w < minW {
+			minW = p.w
+		}
+		if p.w > maxW {
+			maxW = p.w
+		}
+	}
+
+	for z := minZ - 1; z <= maxZ+1; z++ {
+		for y := minY - 1; y <= maxY+1; y++ {
+			for x := minX - 1; x <= maxX+1; x++ {
+				for w := minW - 1; w <= maxW+1; w++ {
+					p := Position{
+						x: x,
+						y: y,
+						z: z,
+					}
+					if withW {
+						p.w = w
+					}
+					if _, ok := (*grid)[p]; !ok {
+						(*grid)[p] = Inactive
+					}
+				}
+			}
+		}
+	}
+}
+
+func Iterate(grid map[Position]State) map[Position]State {
+	newGrid := make(map[Position]State)
+	PadGrid(&grid, true) // Change to false for part 1
+
+	for p, s := range grid {
+		adj := CountAdjacent(&grid, p)
+		newS := s
+		switch s {
+		case Inactive:
+			if adj == 3 {
+				newS = Active
+			}
+		case Active:
+			if !(adj == 2 || adj == 3) {
+				newS = Inactive
+			}
+		}
+
+		newGrid[p] = newS
+	}
+
+	return newGrid
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+
+	grid := make(map[Position]State)
+	var y int
+	for sc.Scan() {
+		for x, char := range sc.Text() {
+			var s State
+			switch char {
+			case '.':
+				s = Inactive
+			case '#':
+				s = Active
+			}
+
+			grid[Position{z: 0, y: y, x: x, w: 0}] = s
+		}
+		y--
+	}
+
+	for i := 0; i < 6; i++ {
+		grid = Iterate(grid)
+	}
+
+	total := CountActive(&grid)
+	fmt.Printf("%d\n", total)
+}
